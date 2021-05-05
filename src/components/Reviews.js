a5dcb15718509626f59351e28f117876c04ec7e7
rehankhan21 <80718912+rehankhan21@users.noreply.github.com>
2021-05-05 13:15:18 -0400

    Add files via upload

---
+++ src/components/Reviews.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+let Reviews = (props) => {
+
+    return (
+        <div className="Reviews">
+            <p>Reviews</p>
+            <p>{props.num}</p>
+        </div>
+    )
+
+}
+
+export default Reviews;
---
+++ src/components/Website_Visitors.js
@@ -0,0 +1,12 @@
+
+let WebsiteVisitors = (props) => {
+
+    return ( 
+    <div className = "WebsiteVisitors">
+        <p>Web Visitors</p>
+        <p>{props.num}</p>
+    </div>
+    )
+}
+
+export default WebsiteVisitors;
